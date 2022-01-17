943abf3254fafd3667cfd8c5659941c7d346735a
hting <26hting@gmail.com>
2022-01-17 16:33:49 +0800

    .

---
+++ pomodoro/src/store/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta: {
+      title: '番茄鐘',
+      keepAlive: true
+    }
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: () => import(/* webpackChunkName: "settings" */ '../views/Settings.vue'),
+    meta: {
+      title: '番茄鐘 | 設定'
+    }
+  },
+  {
+    path: '/task',
+    name: 'Task',
+    component: () => import(/* webpackChunkName: "task" */ '../views/Task.vue'),
+    meta: {
+      title: '番茄鐘 | 事項'
+    }
+  },
+  {
+    path: '/404',
+    name: 'NotFound',
+    component: () => import(/* webpackChunkName: "notfound" */ '../views/NotFound.vue'),
+    meta: {
+      title: '番茄鐘 | 404'
+    }
+  },
+  {
+    path: '*',
+    redirect: '/404'
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+router.afterEach((to) => {
+  document.title = to.meta.title
+})
+
+export default router
